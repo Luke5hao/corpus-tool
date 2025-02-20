77cd0c37cd44b4663e80ce8d68f834c24806e5ca
Davey Seeman <djs467@cornell.edu>
2025-02-20 16:58:03 -0500

    connected to sqlite

--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import index
+from .views import home_view
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', home_view, name='home'),
 ]
--- myproject/home/views.py
+++ myproject/home/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import TextEntry
 
-def index(request):
+def home_view(request):
+    if request.method == 'POST':
+        print("POST data received:", request.POST)  # Debugging line
+        text_input = request.POST.get('text', '')
+        if text_input:
+            TextEntry.objects.create(text=text_input)
+            return redirect('home')
     return render(request, 'home/index.html')
