5148f0862cdf908b634f9bd32825a89e659b8d3b
Luke Shao <lys8@cornell.edu>
2025-02-26 17:36:17 -0500

    added worklog section

--- myproject/home/static/js/app.js
+++ myproject/home/static/js/app.js
@@ -1,6 +1,12 @@
 function updateWordCount() {
-    var text = document.getElementById('textBox').value;
-    var words = text.match(/\b[-?(\w+)?]+\b/gi);
-    var count = words ? words.length : 0;
-    document.getElementById('wordCount').innerText = 'Word count: ' + count;
+    var entry = document.getElementById('textBox').value;
+    var words = entry.match(/\b[-?(\w+)?]+\b/gi);
+    var numWords = words ? words.length : 0;
+    var minWords = 200;
+
+    document.getElementById('wordCount').innerText = 'Word count (minimum 200 words): ' + numWords;
+
+    if(numWords < minWords){
+        submitButton.preventDefault(); 
+    }
 }
